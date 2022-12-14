5a54abc6cedb6fd37524d794a4eaf17b1ef3cdb2
John Rooney <rooneyjohn@gmail.com>
2022-12-13 23:10:29 -0500

    commit tree d5afe7b209a3ddf510d0760b0eefdfc4c7c45ab7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(data) {
+    const frank = data.find(element => element.result == "W")
+    if (frank != undefined) {
+        return frank.year
+    }else {
+        return frank;
+    }
+}
+
+superbowlWin(record);
